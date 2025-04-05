d8d206b15e467c4374f7adde5b905893fea4e381
peterhamilton33 <hamiltonpeter02@gmail.com>
2025-04-04 19:48:32 -0600

    redoing lots

--- backend/backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f238df6ba3db4e025ca606107be2a678cf07c5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95c8954a02a81f217e1cc1d37633600c0c0213e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
